6ae50e2d37e3fa34c794624461450cce40969baf
merkuriospam <merkuriospam@gmail.com>
2016-11-11 16:19:17 -0300

    con muneca

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,5 +1,8 @@
 //var BASE_URL = 'http://localhost/plopi/proyectos/amies/';
 var BASE_URL = 'http://amies.com.ar/';
+
+var img_cuerpo = new Image();
+
 var app = {
     init: function() {
         var self = this;
@@ -18,12 +21,25 @@ var app = {
 		    },
 		    success: function( response ) {
                 $('#mensaje').html(response.rta);
+                app.getImage();
 		    },
 		    error: function(xhr, status, error) {
 		        alert("Ocurrió un Error.");
 		    }
 		});
     },
+    getImage: function() {
+        var self = this;
+        console.log('Iniciando getImage...');
+
+        img_cuerpo.onload = app.cargado;
+        img_cuerpo.src = BASE_URL + "assets/images/amies/demo/cuerpo_completo.png";
+        $('#cuerpo').attr('src', img_cuerpo.src);
+    },
+    cargado: function() {
+        var self = this;
+        console.log('Cargue');
+    },    
 };
 // With jQuery, you use $(document).ready() to execute something when the DOM is loaded 
 // and $(window).on("load", handler) to execute something when all other things are loaded as well, such as the images.
